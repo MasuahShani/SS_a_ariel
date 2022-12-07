52dfab1a33f2a10932427ddbae134df82f70a1bd
MasuahShani <118990932+MasuahShani@users.noreply.github.com>
2022-12-07 23:04:44 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,45 +1,28 @@
 CC=gcc
 AR=ar
 OBJECTS_MAIN=main.o
-OBJECTS_LOOP=advancedClassificationLoop.o BasicClassification.o
-OBJECTS_REC=advancedClassificationRecursion.o BasicClassification.o
+OBJECTS_LIB=my_mat.o
 FLAGS= -Wall -g
 
-all:  loops recursives loopd recursived mains maindloop maindrec
 
-loops: $(OBJECTS_LOOP)
-	$(AR) -rcs libclassloop.a $(OBJECTS_LOOP)
+all:  my_mat connections
 
-recursives: $(OBJECTS_REC)
-	$(AR) -rcs libclassrec.a $(OBJECTS_REC)
+connections: $(OBJECTS_MAIN) my_mat.a 
+	$(CC) $(FLAGS) -o connections $(OBJECTS_MAIN) my_mat.a
 
-loopd: $(OBJECTS_LOOP)
-	$(CC) -fPIC -shared -o libclassloop.so $(OBJECTS_LOOP) 
+my_mat: $(OBJECTS_LIB)
+	$(AR) -rcs my_mat.a $(OBJECTS_LIB)
 
-recursived: $(OBJECTS_REC)
-	$(CC) -fPIC -shared -o libclassrec.so $(OBJECTS_REC) 
+my_mat.o: my_mat.h my_mat.c
+	$(CC) $(FLAGS) -c my_mat.c
 
-mains: $(OBJECTS_MAIN) libclassrec.a 
-	$(CC) $(FLAGS) -o mains $(OBJECTS_MAIN) libclassrec.a
-
-maindloop: $(OBJECTS_MAIN) libclassloop.so
-	$(CC) $(FLAGS) -o maindloop $(OBJECTS_MAIN) ./libclassloop.so
-
-maindrec: $(OBJECTS_MAIN) libclassrec.so
-	$(CC) $(FLAGS) -o maindrec $(OBJECTS_MAIN) ./libclassrec.so
+main.o: my_mat.h main.c
+	$(CC) $(FLAGS) -c main.c
 
 
-main.o: NumClass.h main.c
-	$(CC) $(FLAGS) -c main.c 
 
-BasicClassification.o:NumClass.h BasicClassification.c 
-	$(CC) $(FLAGS) -c BasicClassification.c
 
-advancedClassificationLoop.o:NumClass.h advancedClassificationLoop.c 
-	$(CC) $(FLAGS) -c advancedClassificationLoop.c
 
-advancedClassificationRecursion.o:NumClass.h advancedClassificationRecursion.c 
-	$(CC) $(FLAGS) -c advancedClassificationRecursion.c
 
 
 
@@ -47,4 +30,4 @@ advancedClassificationRecursion.o:NumClass.h advancedClassificationRecursion.c
 .PHONY: clean all
 
 clean:
-	rm -f *.o *.a *.so mains maindloop maindrec
+	rm -f *.o *.a connections 
--- main.c
+++ main.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
-#include "NumClass.h"
+#include <math.h>
+#include "my_mat.h"
 
 int main()
 {
-    int x, y;
-    while(scanf("%d%d", &x, &y) != 2)
+    float i,j;
+    float arr [10][10]={};
+    char ans='D';
+
+    //printf("please enter a char (A-D)");
+    scanf("%c", &ans);
+    
+    while(ans!='D')
     {
-        char c;
-        scanf("%c", &c);
+        if(ans=='A')
+        {
+        //printf("please enter the values of the matrix");
+       
+        matrix(&arr[0][0]);
+            
+        }
+
+        if(ans=='B')
+        {
+        //printf("please enter i and j");
+        scanf("%f %f", &i, &j);
+       
+        int response=is_exists(i,j,arr);
+        if(response==1)
+        {
+            printf("true");
+        }
+        else
+        {
+            printf("false");
+        }
+        
+           
+        }
+        
+        if(ans=='C')
+        {
+            //printf("please enter i and j");
+            scanf("%f %f", &i, &j);
+
+        float response=short_path(i,j,arr);
+        printf("%f", response);
+
+        }
+
+        //printf("please enter a char (A-D)");
+        scanf("%c", &ans);
+        
+
     }
 
-    int big = y, small = x;
-    if(x > y)
-    {
-        small = y;
-        big = x;
-    }
-    printf("The Armstrong numbers are:");
-    for(int i = small ; i <= big; i++ )
-    {
-        if(isArmstrong(i) == 1)
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\nThe Palindromes are:");
-    for(int i = small ; i <= big; i++ )
-    {
-        if(isPalindrome(i) == 1)
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\nThe Prime numbers are:");
-    for(int i = small ; i <= big; i++ )
-    {
-        if(isPrime(i) == 1)
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\nThe Strong numbers are:");
-    for(int i = small ; i <= big; i++ )
-    {
-        if(isStrong(i) == 1)
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
+  
     return 0;
 }
